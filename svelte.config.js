c76d6babb49ca6e739148e2e06cb6868cf24061e
ysr9029 <56439343+Nich87@users.noreply.github.com>
2023-08-29 00:56:02 +0900

    feat:Support PaaS

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,44 @@
 import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import {markdown} from 'svelte-preprocess-markdown';
 
+import adapterNode from '@sveltejs/adapter-node';
+import adapterStatic from '@sveltejs/adapter-static';
+import adapterVercel from '@sveltejs/adapter-vercel';
+import adapterNetlify from '@sveltejs/adapter-netlify';
+import adapterCloudflare from '@sveltejs/adapter-cloudflare';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess(), preprocess({
+  	preprocess: [vitePreprocess(), preprocess({
         postcss: true
     }),
 		markdown()
 	],
-
-	kit: {
-		adapter: adapter({
-            runtime: 'nodejs18.x'
-        })
-	}
+  kit: {
+    adapter: getAdapter({
+		 runtime: 'nodejs18.x'
+	}),
+    csp: { mode: 'auto' },
+  },
 };
+
+function getAdapter() {
+  if (Object.keys(process.env).some((key) => key.includes('VERCEL'))) {
+    return adapterVercel();
+  } else if (Object.keys(process.env).some((key) => key.includes('NETLIFY'))) {
+    return adapterNetlify();
+  } else if (Object.keys(process.env).some((key) => key.includes('CF_PAGES'))) {
+    return adapterCloudflare();
+  } else {
+    return process.env.ADAPTER === 'node'
+      ? adapterNode({ out: 'build' })
+      : adapterStatic({
+          pages: 'build',
+          assets: 'build',
+          fallback: null,
+        });
+  }
+}
 
 export default config;
